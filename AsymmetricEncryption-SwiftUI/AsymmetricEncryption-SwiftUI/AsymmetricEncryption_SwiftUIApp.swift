75756845375c9c297d796911fa08f76ed0bd3d9d
Glenn Posadas <hello@glennvon.com>
2022-04-08 21:24:00 +0800

    add ios proj

---
+++ AsymmetricEncryption-SwiftUI/AsymmetricEncryption-SwiftUI/AsymmetricEncryption_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsymmetricEncryption_SwiftUIApp.swift
+//  AsymmetricEncryption-SwiftUI
+//
+//  Created by Glenn Posadas on 4/8/22.
+//
+
+import SwiftUI
+
+@main
+struct AsymmetricEncryption_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
