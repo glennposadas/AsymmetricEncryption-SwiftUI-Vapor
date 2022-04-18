48ffee25b9936f8ba7013b0e49570dde0ebc5740
Glenn Posadas <hello@glennvon.com>
2022-04-18 16:22:44 +0800

    save stuff.

--- AsymmetricEncryption-SwiftUI/AsymmetricEncryption-SwiftUI/ContentView.swift
+++ AsymmetricEncryption-SwiftUI/AsymmetricEncryption-SwiftUI/ContentView.swift
@@ -5,8 +5,11 @@
 //  Created by Glenn Posadas on 4/8/22.
 //
 
+import SwiftKeychainWrapper
 import SwiftyRSA
 import SwiftUI
+
+let KEYCHAIN = KeychainWrapper(serviceName: Bundle.main.bundleIdentifier! + ".RSA", accessGroup: "publicKey")
 
 struct ContentView: View {
   
@@ -28,16 +31,82 @@ struct ContentView: View {
   }
 }
 
+let keychainPubliKeyKey = "com.glennposadas.AsymmetricEncryption-SwiftUI.publickey"
+let keychainPrivateKeyKey = "com.glennposadas.AsymmetricEncryption-SwiftUI.privateKey"
+
 class VM: ObservableObject {
   
   @Published var showErrorMessage = false
   @Published var error: Error?
   
+  
+  func newKeyPair() throws -> (privateKey: PrivateKey, publicKey: PublicKey) {
+    try SwiftyRSA.generateRSAKeyPair(sizeInBits: 2048)
+  }
+  
+  func publicKey() throws -> Data {
+    if let stored = KEYCHAIN.data(forKey: keychainPubliKeyKey, isSynchronizable: true) {
+      return stored
+    } else {
+      let publicKeyData = try newKeyPair().publicKey.data()
+      
+      KEYCHAIN.set(publicKeyData,
+                   forKey: keychainPubliKeyKey,
+                   isSynchronizable: true)
+      
+      return data
+    }
+  }
+  
+  func privateKey() throws -> Data {
+    if let stored = KEYCHAIN.data(forKey: keychainPrivateKeyKey, isSynchronizable: true) {
+      
+      return stored
+      
+    } else {
+      
+      // Generate keys
+      let keyPair = try SwiftyRSA.generateRSAKeyPair(sizeInBits: 2048)
+      
+      let privateKey = keyPair.privateKey
+      let privatePem = try privateKey.pemString()
+      
+      let publicKey = keyPair.publicKey
+      let data = try publicKey.data()
+//      let publicPem = try publicKey.pemString()
+      
+      KEYCHAIN.set(data,
+                   forKey: keychainPrivateKeyKey,
+                   isSynchronizable: true)
+      
+      return data
+    }
+  }
+  
   func testAE() {
     do {
-      let publicKey = try PublicKey(pemNamed: "public")
+      
+      let storedPublicKey = try PublicKey(data: publicKey())
       let clear = try ClearMessage(string: "Clear Text", using: .utf8)
-      let encrypted = try clear.encrypted(with: publicKey, padding: .PKCS1)
+      
+      let encrypted = try clear.encrypted(with: storedPublicKey, padding: .PKCS1)
+      
+      let data = encrypted.data
+      let base64String = encrypted.base64String
+      
+      let privateKey = try PrivateKey(pemNamed: "private")
+      let encrypted2 = try EncryptedMessage(base64Encoded: "AAA===")
+      let clear2 = try encrypted.decrypted(with: privateKey, padding: .PKCS1)
+      
+      // Then you can use:
+      let data2 = clear2.data
+      let base64String2 = clear2.base64String
+      let string = try clear2.string(encoding: .utf8)
+      
+      
+      
+      
+      
     } catch {
       self.error = error
       showErrorMessage = true
